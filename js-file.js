e24c639689f47c5150c951be8a2afc91259d4208
Alex Reilly <iamalexreilly@gmail.com>
2024-07-09 15:26:09 -0400

    Make hover functional for etch a sketch
    Create event listener so when mouse is hovering over
    a square, the background changes to a select color.

--- js-file.js
+++ js-file.js
@@ -7,6 +7,7 @@ let currentSize = DEFAULT_SIZE;
 const grid = document.querySelector("#grid-container");
 //const input;
 
+//make the grid with white backgrounds
 function makeGrid(numSquares){
     for(let i = 0; i < numSquares; i++){
         for(let j = 0; j<numSquares; j++){
@@ -20,4 +21,12 @@ function makeGrid(numSquares){
     }
 }
 
+//call make grid function
 makeGrid(currentSize);
+
+//make event listener for all boxes when hovering
+grid.addEventListener("mouseover", (e)=>{
+    if(e.target.classList.contains("box")){
+        e.target.style.backgroundColor = currentColor;
+    }
+});
