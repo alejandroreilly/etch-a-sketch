cab70b4d4617baddbdb338d9e6b86f2dd5c3ea1e
Alex Reilly <iamalexreilly@gmail.com>
2024-07-09 12:25:22 -0400

    Create the basic setup for the webpage
    Initialize the HTML layout, set-up for flexbox use. Initialize
    styles for the grid container and main container for the game.
    Create some default variables we can adjust with user input
    later in the JS file.

---
+++ js-file.js
@@ -0,0 +1,12 @@
+const DEFAULT_COLOR = "black";
+const DEFAULT_SIZE = 16;
+
+let currentColor = DEFAULT_COLOR;
+let currentSize = DEFAULT_SIZE;
+
+const grid = document.querySelector("grid-container");
+//const input;
+
+function makeGrid(numSquares){
+    
+}
