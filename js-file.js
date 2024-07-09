c942be825670d106aecf5b3bf65083ddc6ba2f67
Alex Reilly <iamalexreilly@gmail.com>
2024-07-09 14:54:56 -0400

    Create the grid for the etch-a sketch
    Use JS to create a grid based on a number passed into the function.
    This will be user-defined later on. Edit the box-sizing so that it is
    border box, allowing our elements to fit in their container properly.

--- js-file.js
+++ js-file.js
@@ -4,9 +4,20 @@ const DEFAULT_SIZE = 16;
 let currentColor = DEFAULT_COLOR;
 let currentSize = DEFAULT_SIZE;
 
-const grid = document.querySelector("grid-container");
+const grid = document.querySelector("#grid-container");
 //const input;
 
 function makeGrid(numSquares){
-    
+    for(let i = 0; i < numSquares; i++){
+        for(let j = 0; j<numSquares; j++){
+            //add each box to the grid
+            const box = document.createElement("div");
+            box.style.width = `calc(100% / ${numSquares})`;
+            box.style.height = `calc(100% / ${numSquares})`;
+            box.setAttribute("class","box");
+            grid.appendChild(box);
+        }
+    }
 }
+
+makeGrid(currentSize);
